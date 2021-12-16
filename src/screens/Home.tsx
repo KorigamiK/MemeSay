f3399b9b3026dd2225b53b2e7dab30ce781ddf3d
korigamik win <korigamik@gmail.com>
2021-12-16 17:25:14 +0530

    install cowsay

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { ScreenProps } from '.';
+import cowsay from 'cowsay2';
+import cows from 'cowsay2/cows';
 
 const Home = ({}: ScreenProps<'Home'>) => {
+  const x = cowsay.say('Hello', { cow: cows.eyes });
+
   return (
     <View>
       <Text style={styles.text}>Hi HI</Text>
+      <Text style={styles.text}>{x}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  text: { color: 'green' },
+  text: { color: 'black' },
 });
 export default Home;
